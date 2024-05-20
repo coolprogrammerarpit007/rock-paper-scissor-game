14ff4fcd7e27499370fa4cb8dc7b9117af814bb1
Arpit <arpit.mishra.out@gmail.com>
2024-05-20 17:01:35 +0530

    Game features added

--- js/index.js
+++ js/index.js
@@ -1,33 +1,130 @@
-`use strict`;
+`usee strict`;
 
-// storing the choice buttons
+// Getting Acess to the DOM Elements
+
+const gameResult = document.querySelector(`.game-rules`);
+const userChosenOption = document.getElementById(`userChosen`);
+const userScoreDisplay = document.querySelector(`.user--Score`);
+const computerChosenOption = document.getElementById(`computerChosen`);
+const computerScoreDisplay = document.querySelector(`.computer--Score`);
 
 const rockBtn = document.getElementById(`choice-1`);
 const paperBtn = document.getElementById(`choice-2`);
 const scissorBtn = document.getElementById(`choice-3`);
-const userChosen = document.getElementById(`userChosen`);
-const computerChosen = document.getElementById(`computerChosen`);
 
 // state variables
 let userScore = 0;
 let computerScore = 0;
-const arr = [`rock`, `paper`, `scissor`];
-const gameChoices = {
+const gameArr = [`rock`, `paper`, `scissor`];
+const gameObj = {
   0: `✊`,
   1: `✋`,
   2: `✌️`,
 };
 
-// function to check who wins!
+// *******************************
+// *******************************
 
-const checkWinner = function (userChosen, computerChosen) {};
-rockBtn.addEventListener(`click`, function (e) {
-  let randomChoice = Math.floor(Math.random() * 3);
-  computerChosen.textContent = gameChoices[randomChoice];
-  userChosen.textContent = gameChoices[0];
-  let userChosenOption = `rock`;
-  let computerChosenOption = arr[randomChoice];
-  checkWinner(userChosenOption, computerChosenOption);
+// function to update DOM
+
+const updateDOM = function (score1, score2) {
+  userScoreDisplay.textContent = score1;
+  computerScoreDisplay.textContent = score2;
+};
+
+// function to update game choice
+
+const updateChoice = function (num1, num2) {
+  userChosenOption.textContent = gameObj[num1];
+  computerChosenOption.textContent = gameObj[num2];
+};
+
+// function to randomally generate number
+const randomGenerateNumber = function () {
+  return Math.floor(Math.random() * gameArr.length);
+};
+
+// Event listener to the rock btn
+
+const rockChoice = function (user, machine) {
+  if (user === machine) {
+    updateDOM(userScore, computerScore);
+    console.log(`Match Tied!`);
+  } else if ((user === `rock`, machine === `paper`)) {
+    computerScore++;
+    updateDOM(userScore, computerScore);
+    console.log(`Computer Wins!`);
+  } else if ((user === `rock`, machine === `scissor`)) {
+    userScore++;
+    updateDOM(userScore, computerScore);
+    console.log(`User Wins!`);
+  }
+};
+
+rockBtn.addEventListener(`click`, function () {
+  let randomNum = randomGenerateNumber();
+  let userOpts = `rock`;
+  let computerOpts = gameArr[randomNum];
+
+  updateChoice(0, randomNum);
+  rockChoice(userOpts, computerOpts);
 });
-paperBtn.addEventListener(`click`, function (e) {});
-scissorBtn.addEventListener(`click`, function (e) {});
+
+// ************************************
+// ************************************
+// ************************************
+// ************************************
+
+const paperChoice = function (user, machine) {
+  if (user === machine) {
+    updateDOM(userScore, computerScore);
+    console.log(`Match Tied!`);
+  } else if ((user === `paper`, machine === `rock`)) {
+    userScore++;
+    updateDOM(userScore, computerScore);
+    console.log(`User Wins!`);
+  } else if ((user === `paper`, machine === `scissor`)) {
+    computerScore++;
+    updateDOM(userScore, computerScore);
+    console.log(`Computer Wins!`);
+  }
+};
+
+// Adding event listener to the paper button
+paperBtn.addEventListener(`click`, function () {
+  let randomNum = randomGenerateNumber();
+  let userOpts = `paper`;
+  let computerOpts = gameArr[randomNum];
+  updateChoice(1, randomNum);
+  paperChoice(userOpts, computerOpts);
+});
+
+// Adding Event Listener to the scissor button
+
+const scissorChoice = function (user, machine) {
+  if (user === machine) {
+    updateDOM(userScore, computerScore);
+    console.log(`Match Tied!`);
+  } else if ((user === `scissor`, machine === `rock`)) {
+    computerScore++;
+    updateDOM(userScore, computerScore);
+    console.log(`Computer Wins!`);
+  } else if ((user === `scissor`, machine === `paper`)) {
+    userScore++;
+    updateDOM(userScore, computerScore);
+    console.log(`User Wins!`);
+  }
+};
+
+scissorBtn.addEventListener(`click`, function () {
+  let randomNum = randomGenerateNumber();
+  let userOpts = `scissor`;
+  let computerOpts = gameArr[randomNum];
+  updateChoice(2, randomNum);
+  scissorChoice(userOpts, computerOpts);
+});
+
+// ***********************************
+// ***********************************
+
+// Check Winner Functuanility to the amazon cart
