004b10d7d1b73b0f5eff67bd0c01b7ce84e741d1
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-03 07:37:53 +0530

    rock papper scissor updated game

--- js/index.js
+++ js/index.js
@@ -1,180 +1,172 @@
-`usee strict`;
+`use strict`;
 
-// Getting Acess to the DOM Elements
+// static variables
 
-const gameResult = document.getElementById(`score--winner`);
-const userChosenOption = document.getElementById(`userChosen`);
-const userScoreDisplay = document.querySelector(`.user--Score`);
-const computerChosenOption = document.getElementById(`computerChosen`);
-const computerScoreDisplay = document.querySelector(`.computer--Score`);
+let userScore = 0;
+let computerScore = 0;
+let humanChoice;
+let computerChoice;
+let isPlaying = true;
+let winnerGamePlayer = ``;
+
+// storing game buttons
 
 const rockBtn = document.getElementById(`choice-1`);
 const paperBtn = document.getElementById(`choice-2`);
 const scissorBtn = document.getElementById(`choice-3`);
 
-// state variables
-let userScore = 0;
-let computerScore = 0;
-const gameArr = [`rock`, `paper`, `scissor`];
-const gameObj = {
-  0: `✊`,
-  1: `✋`,
-  2: `✌️`,
-};
+// getting the dom eements
+const userChosen = document.getElementById(`userChosen`);
+const computerChosen = document.getElementById(`computerChosen`);
+const userScoreEl = document.getElementById(`userScore`);
+const computerScoreEl = document.getElementById(`computerScore`);
 
-// *******************************
-// *******************************
-
-// function to update DOM
-
-const updateDOM = function (score1, score2) {
-  userScoreDisplay.textContent = score1;
-  computerScoreDisplay.textContent = score2;
-};
-
-// function to update game choice
-
-const updateChoice = function (num1, num2) {
-  userChosenOption.textContent = gameObj[num1];
-  computerChosenOption.textContent = gameObj[num2];
-};
-
-// function to randomally generate number
-const randomGenerateNumber = function () {
-  return Math.floor(Math.random() * gameArr.length);
-};
-
-// Event listener to the rock btn
-
-const rockChoice = function (user, machine) {
-  if (user === machine) {
-    updateDOM(userScore, computerScore);
-    gameResult.textContent = `Match Tied!`;
-    console.log(`Match Tied!`);
-  } else if ((user === `rock`, machine === `paper`)) {
-    computerScore++;
-    updateDOM(userScore, computerScore);
-    gameResult.textContent = `Computer Wins!`;
-    console.log(`Computer Wins!`);
-  } else if ((user === `rock`, machine === `scissor`)) {
-    userScore++;
-    updateDOM(userScore, computerScore);
-    console.log(`User Wins!`);
-    gameResult.textContent = `User Wins!`;
-  }
-  checkWinner();
-};
-
-rockBtn.addEventListener(`click`, function () {
-  let randomNum = randomGenerateNumber();
-  let userOpts = `rock`;
-  let computerOpts = gameArr[randomNum];
-
-  updateChoice(0, randomNum);
-  rockChoice(userOpts, computerOpts);
-});
-
-// ************************************
-// ************************************
-// ************************************
-// ************************************
-
-const paperChoice = function (user, machine) {
-  if (user === machine) {
-    updateDOM(userScore, computerScore);
-    console.log(`Match Tied!`);
-    gameResult.textContent = `Match Tied!`;
-  } else if ((user === `paper`, machine === `rock`)) {
-    userScore++;
-    updateDOM(userScore, computerScore);
-    gameResult.textContent = `User Wins!`;
-    console.log(`User Wins!`);
-  } else if ((user === `paper`, machine === `scissor`)) {
-    computerScore++;
-    updateDOM(userScore, computerScore);
-    gameResult.textContent = `Computer Wins!`;
-    console.log(`Computer Wins!`);
-  }
-  checkWinner();
-};
-
-// Adding event listener to the paper button
-paperBtn.addEventListener(`click`, function () {
-  let randomNum = randomGenerateNumber();
-  let userOpts = `paper`;
-  let computerOpts = gameArr[randomNum];
-  updateChoice(1, randomNum);
-  paperChoice(userOpts, computerOpts);
-});
-
-// Adding Event Listener to the scissor button
-
-const scissorChoice = function (user, machine) {
-  if (user === machine) {
-    updateDOM(userScore, computerScore);
-    gameResult.textContent = `Match Tied!`;
-    console.log(`Match Tied!`);
-  } else if ((user === `scissor`, machine === `rock`)) {
-    computerScore++;
-    updateDOM(userScore, computerScore);
-    gameResult.textContent = `Computer Wins!`;
-    console.log(`Computer Wins!`);
-  } else if ((user === `scissor`, machine === `paper`)) {
-    userScore++;
-    updateDOM(userScore, computerScore);
-    gameResult.textContent = `User Wins!`;
-    console.log(`User Wins!`);
-  }
-  checkWinner();
-};
-
-scissorBtn.addEventListener(`click`, function () {
-  let randomNum = randomGenerateNumber();
-  let userOpts = `scissor`;
-  let computerOpts = gameArr[randomNum];
-  updateChoice(2, randomNum);
-  scissorChoice(userOpts, computerOpts);
-});
-
-// ***********************************
-// ***********************************
-
-// Check Winner Functuanility to the amazon cart
-
-const updateDOMWinner = function () {
-  document.querySelector(`.computer`).classList.add(`hidden`);
-  document.querySelector(`.user`).classList.add(`hidden`);
-  document.querySelector(`.winner`).classList.remove(`hidden`);
-};
-
-const checkWinner = function () {
-  if (userScore === 5) {
-    document.getElementById(`winner-player`).textContent = `User won the game`;
-    console.log(`User won the game`);
-    updateDOMWinner();
-  } else if (computerScore === 5) {
-    console.log(`computer won the game`);
-    document.getElementById(
-      `winner-player`
-    ).textContent = `Computer won the game`;
-    updateDOMWinner();
-  }
-};
-
-// getting acess to the playAgain Button
-
+const winner = document.querySelector(`.winner`);
+const winnerPlayer = document.getElementById(`winner-player`);
 const playAgainBtn = document.querySelector(`.again`);
+const userPlayer = document.querySelector(`.user`);
+const machinePlayer = document.querySelector(`.computer`);
+const scoreWinner = document.getElementById(`score--winner`);
+// choice objs
+let choices = {
+  rock: `✊`,
+  paper: `✋`,
+  scissor: `✌️`,
+};
+// console.log(choices);
+// function to get random computer choice
 
-// Adding event listener to playAgain button to make it to the default settings to the page.
-playAgainBtn.addEventListener(`click`, function () {
-  document.querySelector(`.computer`).classList.remove(`hidden`);
-  document.querySelector(`.user`).classList.remove(`hidden`);
-  document.querySelector(`.winner`).classList.add(`hidden`);
+const getComputerChoice = () => {
+  const choices = [`rock`, `paper`, `scissor`];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+};
+
+// function to get the user choice
+
+const userChoice = (choice) => {
+  // console.log(choice);
+  return choice;
+};
+
+// function to play a single round
+
+const playRound = (humanChoice, computerChoice) => {
+  // logic to win the round
+
+  if (humanChoice === computerChoice) {
+    scoreWinner.textContent = `Match Tied!`;
+  } else if (humanChoice === `rock` && computerChoice === `scissor`) {
+    scoreWinner.textContent = `User Won this Round!`;
+    winnerGamePlayer = `User`;
+
+    userScore++;
+  } else if (humanChoice === `paper` && computerChoice === `rock`) {
+    scoreWinner.textContent = `User Won this Round!`;
+    winnerGamePlayer = `user`;
+
+    userScore++;
+  } else if (humanChoice === `scissor` && computerChoice === `paper`) {
+    scoreWinner.textContent = `User Won this Round!`;
+    winnerGamePlayer = `user`;
+
+    userScore++;
+  } else {
+    scoreWinner.textContent = `Computer Won this Round!`;
+    winnerGamePlayer = `computer`;
+
+    computerScore++;
+  }
+
+  // calling function to update the dom
+  updateDOM(userScore, computerScore, humanChoice, computerChoice);
+};
+
+// function to update the dom
+
+const updateDOM = function (
+  userScore,
+  computerScore,
+  humanChoice,
+  computerChoice
+) {
+  // Updating the DOM
+  // updating the choices
+  userChosen.innerHTML = choices[humanChoice];
+  computerChosen.innerHTML = choices[computerChoice];
+
+  // updating the score
+  userScoreEl.textContent = userScore;
+  computerScoreEl.textContent = computerScore;
+};
+
+// Adding the event listener to all the buttons
+
+// calling function playRound
+rockBtn.addEventListener(`click`, (e) => {
+  console.log(userScore, computerScore);
+  if (userScore < 5 && computerScore < 5) {
+    humanChoice = userChoice(`rock`);
+    computerChoice = getComputerChoice();
+
+    // calling function playRound
+    playRound(humanChoice, computerChoice);
+  } else {
+    isPlaying = false;
+    winner.classList.remove(`hidden`);
+    userPlayer.classList.add(`hidden`);
+    machinePlayer.classList.add(`hidden`);
+    winnerPlayer.textContent = `${winnerGamePlayer} won the Game!`;
+  }
+});
+paperBtn.addEventListener(`click`, (e) => {
+  if (userScore < 5 && computerScore < 5) {
+    humanChoice = userChoice(`paper`);
+    computerChoice = getComputerChoice();
+
+    // calling function playRound
+    playRound(humanChoice, computerChoice);
+  } else {
+    isPlaying = false;
+    winner.classList.remove(`hidden`);
+    userPlayer.classList.add(`hidden`);
+    machinePlayer.classList.add(`hidden`);
+    winnerPlayer.textContent = `${winnerGamePlayer} won the Game!`;
+  }
+});
+scissorBtn.addEventListener(`click`, (e) => {
+  if (userScore < 5 && computerScore < 5) {
+    humanChoice = userChoice(`scissor`);
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  } else {
+    isPlaying = false;
+    winner.classList.remove(`hidden`);
+    userPlayer.classList.add(`hidden`);
+    machinePlayer.classList.add(`hidden`);
+    winnerPlayer.textContent = `${winnerGamePlayer} won the Game!`;
+  }
+});
+
+// play again functuanility
+
+const playAgain = function () {
+  isPlaying = true;
   userScore = 0;
   computerScore = 0;
-  userScoreDisplay.textContent = userScore;
-  computerScoreDisplay.textContent = computerScore;
-  userChosenOption.textContent = `❓`;
-  computerChosenOption.textContent = `❓`;
-  gameResult.textContent = `First to score 5 points win the game`;
-});
+  scoreWinner.textContent = `First to score 5 points win the game`;
+  winner.classList.add(`hidden`);
+  userPlayer.classList.remove(`hidden`);
+  machinePlayer.classList.remove(`hidden`);
+
+  // Updating the DOM
+  // updating the choices
+  userChosen.innerHTML = `❓`;
+  computerChosen.innerHTML = `❓`;
+
+  // updating the score
+  userScoreEl.textContent = userScore;
+  computerScoreEl.textContent = computerScore;
+};
+
+playAgainBtn.addEventListener(`click`, playAgain);
